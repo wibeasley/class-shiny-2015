3d8bea29b80ea306dd77d8ea9302e477240d1540
dwellsOKC <donna-wells@ouhsc.edu>
2015-04-24 11:13:12 -0500

    lesson2
    lesson2

---
+++ Donna/lesson2/ui.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+shinyUI
+(fluidPage
+  (
+  titlePanel("Weather Forecast"),
+  sidebarLayout
+    (
+      sidebarPanel
+      (
+        h2(em("7 Day",style="color:blue")),
+        h3(strong("100 % Guarantee",style="color:red"))
+      ),
+                        
+    mainPanel
+    (
+      h1("The rain",style="font-family: 'Helvetica'"),
+      img(src="39.png",height=100,width=100),
+      h2("In Spain", style="color:red"),
+      br(),
+      img(src="Spain.png",height=100,width=100),
+      h3("Stays",span("mainly",style="color:green"),"in the plains."),
+      br(),
+      h1("But",span(strong("everywhere",style="color:orange"))),
+      br(),
+      h2("In",span(strong("Oklahoma!!!",style="color:red")))
+      
+    )
+  )
+))
+
