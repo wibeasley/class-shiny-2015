5d21c7d37e9d3b5931056d79b0dd4c883ecd3c07
Will Beasley <wibeasley@hotmail.com>
2015-04-27 11:34:18 -0500

    experimenting with CSS in class

--- Thomas/lesson_2/ui.R
+++ Thomas/lesson_2/ui.R
@@ -3,18 +3,35 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  titlePanel("title panel", img(src="stopsign.jpg", height = 25, width = 25, align = "left")),
+  shiny::tags$head(
+    # includeCSS("./www/styles.css"), # Include our custom CSS
+  
+      tags$style('
+        h1 { color:#7D64FF; }
+        .accent { color:#7D64FF;  }
+        # body { background-image:url("stopsign.jpg"); }
+        .outcome { text-align:right; font-size:300%";}
+      ')  #Closes tags$style
+  
+  ), #Closes tags$head  
+  titlePanel("title panel", img(src="./stopsign.jpg", height = 25, width = 25, align = "left")),
   
   #position options
   sidebarLayout(position = "left", #default
   #sidebarLayout(position = "right",
+  
+
+
     sidebarPanel("sidebar panel",
-                 img(src="stopsign.jpg", height = 25, width = 25, align = "left")),
+                 img(src="./stopsign.jpg", height = 25, width = 25, align = "left")),
     mainPanel(
       h6("Title 1", align = "center"),
       p("this is some really important text right here,"),
       p("but it won't fit on 1 line!"),
       p("To use REDCap go to", style = "color:red; font-family: 'Brush Script MT'; font-size:300%"),
+      p("Outcome Only", class = "outcome"),
+      p("Accent Only", class = "accent"),
+      p("Outcome and Accent", class = "outcome accent"),
       a("https://bbmc.ouhsc.edu"),
       br(),
       h5("Title 2", align = "right"),
@@ -26,8 +43,8 @@ shinyUI(fluidPage(
       h2("Title 5", align = "center"),
       br(),
       h1("Title 6", align = "right"),
-      #img src="https://miechvprojects.ouhsc.edu/redcap/redcap_v4.11.2/MIECHV/stopsign.jpg" alt="stopsign"
-      img(src="stopsign.jpg", height = 200, width = 200, align = "left")
+      # img src="https://miechvprojects.ouhsc.edu/redcap/redcap_v4.11.2/MIECHV/stopsign.jpg" alt="stopsign"
+      img(src="./stopsign.jpg", height = 200, width = 200, align = "left", alt="stopsign")
       )
   )
 ))
