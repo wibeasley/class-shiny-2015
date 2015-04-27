7392de6225e23d100715a3e0c0141aa030e53c58
Thomas <thomas-wilson@ouhsc.edu>
2015-04-27 09:23:54 -0500

    lesson_2
    files_related_to_the_somewhat_successful_completion_of_shiney_lesson_2_plus_the_renaming_of_folders_and_files_to_follow_the_naming_conventions_set_forth_by_will

---
+++ Thomas/lesson_2/ui.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  titlePanel("title panel", img(src="stopsign.jpg", height = 25, width = 25, align = "left")),
+  
+  #position options
+  sidebarLayout(position = "left", #default
+  #sidebarLayout(position = "right",
+    sidebarPanel("sidebar panel",
+                 img(src="stopsign.jpg", height = 25, width = 25, align = "left")),
+    mainPanel(
+      h6("Title 1", align = "center"),
+      p("this is some really important text right here,"),
+      p("but it won't fit on 1 line!"),
+      p("To use REDCap go to", style = "color:red; font-family: 'Brush Script MT'; font-size:300%"),
+      a("https://bbmc.ouhsc.edu"),
+      br(),
+      h5("Title 2", align = "right"),
+      br(),
+      h4("Title 3", align = "center"),
+      br(),
+      h3("Title 4", align = "left"),
+      br(),
+      h2("Title 5", align = "center"),
+      br(),
+      h1("Title 6", align = "right"),
+      #img src="https://miechvprojects.ouhsc.edu/redcap/redcap_v4.11.2/MIECHV/stopsign.jpg" alt="stopsign"
+      img(src="stopsign.jpg", height = 200, width = 200, align = "left")
+      )
+  )
+))
