53a0a918bce4899ddd85227b4c2fa277690dfdc7
Zabrina <zabrina-antry@ouhsc.edu>
2015-05-04 10:11:59 -0500

    Lesson 3
    My turn.  Created censusVis

--- zabrina/lesson_2/ui.R
+++ zabrina/lesson_2/ui.R
@@ -1,71 +1,18 @@
-# Lesson 3
+# Lesson 3 My Turn
 
 shinyUI(fluidPage(
-  titlePanel("Basic Widgets"),
-  fluidRow(
-    column(3,
-       h3("Buttons"),
-       actionButton("action", label = "Action"),
-       br(),
-       br(),
-       submitButton("Submit")),
-    column(3,
-       h3("Single checkbox"),
-       checkboxInput("checkbox", label = "Choice A", value = TRUE)),
-    column(3,
-       checkboxGroupInput("checkGroup",
-        label = h3("Checkbox Group"),
-        choices = list("Choice 1" = 1,
-          "Choice 2" = 2, "Choice 3" = 3),
-        selected = 1)),
-    column(3, 
-           dateInput("date", 
-                     label = h3("Date input"), 
-                     value = "2014-01-01"))   
+  titlePanel("censusVis"),
+  sidebarLayout(
+  sidebarPanel(
+    helpText("Create demographic maps with information from the 2010 United States Census."),
+    selectInput("var",
+    label = "Choose a variable to display:",
+    choices = list("Percent White", "Percent Black", "Percent Hispanic", "Percent Asian"),
+    selected = "Percent White"),
+    sliderInput("range",
+      label = "Range of Interest:",
+      min = 0, max = 100, value = c(0, 100))
   ),
-  
-  fluidRow(
-    
-    column(3,
-           dateRangeInput("dates", label = h3("Date range"))),
-    
-    column(3,
-           fileInput("file", label = h3("File input"))),
-    
-    column(3, 
-           h3("Help text"),
-           helpText("Note: help text isn't a true widget,", 
-                    "but it provides an easy way to add text to",
-                    "accompany other widgets.")),
-    
-    column(3, 
-           numericInput("num", 
-                        label = h3("Numeric input"), 
-                        value = 1))   
-  ),
-  
-  fluidRow(
-    
-    column(3,
-           radioButtons("radio", label = h3("Radio buttons"),
-                        choices = list("Choice 1" = 1, "Choice 2" = 2,
-                                       "Choice 3" = 3),selected = 1)),
-    
-    column(3,
-           selectInput("select", label = h3("Select box"), 
-                       choices = list("Choice 1" = 1, "Choice 2" = 2,
-                                      "Choice 3" = 3), selected = 1)),
-    
-    column(3, 
-           sliderInput("slider1", label = h3("Sliders"),
-                       min = 0, max = 100, value = 50),
-           sliderInput("slider2", "",
-                       min = 0, max = 100, value = c(25, 75))
-    ),
-    
-    column(3, 
-           textInput("text", label = h3("Text input"), 
-                     value = "Enter text..."))   
+  mainPanel()
   )
-  
 ))
