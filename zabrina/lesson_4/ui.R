bb422baa65a97e269e6928b8e4d6d33059b091b4
Will Beasley <wibeasley@hotmail.com>
2015-05-11 11:21:53 -0500

    formatting tabs

--- zabrina/lesson_4/ui.R
+++ zabrina/lesson_4/ui.R
@@ -3,19 +3,19 @@
 shinyUI(fluidPage(
   titlePanel("censusVis"),
   sidebarLayout(
-  sidebarPanel(
-    helpText("Create demographic maps with information from the 2010 United States Census."),
-    selectInput("var",
-    label = "Choose a variable to display:",
-    choices = list("Percent White", "Percent Black", "Percent Hispanic", "Percent Asian"),
-    selected = "Percent White"),
-    sliderInput("range",
-      label = "Range of Interest:",
-      min = 0, max = 100, value = c(0, 100))
-  ),
-  mainPanel(
-    textOutput("text1"),
-    textOutput("rangeval")
-    )
-  )
-))
+    sidebarPanel(
+      helpText("Create demographic maps with information from the 2010 United States Census."),
+      selectInput("var",
+      label = "Choose a variable to display:",
+      choices = list("Percent White", "Percent Black", "Percent Hispanic", "Percent Asian"),
+      selected = "Percent White"),
+      sliderInput("range",
+        label = "Range of Interest:",
+        min = 0, max = 100, value = c(0, 100))
+    ), #sidebarPanel
+    mainPanel(
+      textOutput("text1"),
+      textOutput("rangeval")
+    ) #mainPanel
+  ) #sidebarLayout
+)) #shinyUI & fluidPage
