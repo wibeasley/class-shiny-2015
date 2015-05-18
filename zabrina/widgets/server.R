658ba86b3445ebb9d7803826a0dbda84c2025e89
Will Beasley <wibeasley@hotmail.com>
2015-05-18 12:04:13 -0500

    wrinkles added during class

--- zabrina/widgets/server.R
+++ zabrina/widgets/server.R
@@ -1,6 +1,16 @@
 shinyServer(function(input, output) {
   
-  # You can access the value of the widget with input$checkbox, e.g.
-  output$value <- renderPrint({ input$checkbox })
+  # You can access the value of the widget with input$checkbox_top, e.g.
+  output$value_1 <- renderPrint({ input$checkbox_top })
+  output$value_2 <- renderPrint({ 
+    a <- !input$checkbox_top 
+    return( a )
+  })
+  output$value_3 <- renderPrint({ 
+    # browser()
+    b <- ifelse(input$checkbox_top, "Checked", "Not Checked")
+    # b <- ifelse(!input$checkbox_top, "Not Checked", "Checked")
+    return( b )
+  })
   
 })
--- zabrina/widgets/ui.R
+++ zabrina/widgets/ui.R
@@ -1,9 +1,17 @@
 shinyUI(fluidPage(
   
   # Copy the line below to make a checkbox
-  checkboxInput("checkbox", label = "Choice A", value = TRUE),
+  checkboxInput("checkbox_top", label = "Choice A", value = TRUE),
+  # checkboxInput("check_box", label = "Choice A", value = TRUE),
   
   hr(),
-  fluidRow(column(3, verbatimTextOutput("value")))
+  hr(),
+  hr(),
+  # verbatimTextOutput(outputId="value_1")
+  fluidRow(
+    column(width=2, verbatimTextOutput("value_1")),
+    column(width=2, verbatimTextOutput("value_2")),
+    column(width=2, verbatimTextOutput("value_3"))
+  )
   
 ))
