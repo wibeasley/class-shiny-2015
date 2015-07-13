364950a039a627209cd97cbcf390efac7d72e91f
sbohora <som-bohora@ouhsc.edu>
2015-07-13 12:01:46 -0500

    R file for Github examples practice

---
+++ Som/RFileForGithubExamplesPractice/practice.R
@@ -0,0 +1,15 @@
+rm(list = ls(all=TRUE))
+
+value = 6
+
+if (value < 5){
+  print ("The value is < 5")
+  
+} else if (value > 2 & value < 4){
+    
+  print ("The value is between 2 and 4")
+  
+} else {print ("The value is > 5")
+  
+}
+
